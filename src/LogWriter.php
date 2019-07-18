adda6e0b5b869869018d826a7d560c0e8400f618
Al Imran Ahmed <al.imran.cse@gmail.com>
2019-07-18 16:29:31 +0600

    Fixed: Response type hint removed as it causes problem for json response

--- src/LogWriter.php
+++ src/LogWriter.php
@@ -5,11 +5,10 @@ namespace AlImranAhmed\HitLogger;
 
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 interface LogWriter
 {
     public function logRequest(Request $request);
 
-    public function logResponse(Request $request, Response $response);
+    public function logResponse(Request $request, $response);
 }
